24ee2d8d01a506334acc4e9362a463c24c7a48db
skinrush <contact@hyperdesign.agency>
2025-04-11 22:12:53 +0100

    Fix: mapped model fields to DB column names

--- models/Skin.js
+++ models/Skin.js
@@ -2,12 +2,13 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../db');
 
 const Skin = sequelize.define('Skin', {
+  id: { type: DataTypes.STRING, field: 'id' },
   name: { type: DataTypes.STRING, field: 'name' },
   weapon: { type: DataTypes.STRING, field: 'weapon' },
   collection: { type: DataTypes.STRING, field: 'collection' },
   rarity: { type: DataTypes.STRING, field: 'rarity' },
-  minFloat: { type: DataTypes.FLOAT, field: 'minfloat' },
-  maxFloat: { type: DataTypes.FLOAT, field: 'maxfloat' },
+  min_float: { type: DataTypes.FLOAT, field: 'min_float' },
+  max_float: { type: DataTypes.FLOAT, field: 'max_float' },
   stattrak: { type: DataTypes.BOOLEAN, field: 'stattrak' },
   souvenir: { type: DataTypes.BOOLEAN, field: 'souvenir' }
 }, {
